4c16acba685735ad98768eab121d0094635730d8
md kamruzzamanm <kamruzzaman@triplewsols.orgm>
2023-01-21 12:09:56 +0600

    secure wallet is created

---
+++ controller/scureController/index.js
@@ -0,0 +1,48 @@
+const Wallet = require("../../Model/walletSchema");
+
+const createWallet = async (req, res) => {
+  // console.log(req.body.data);
+  try {
+    const { walletAddress, requestAmount, requestType, time } = req.body.data;
+
+    if (!walletAddress || !requestAmount || !requestType || !time) {
+      return res.status(422).json({ error: "please filled properly" });
+    }
+
+    const WalletExist = await Wallet.findOne({ walletAddress: walletAddress });
+
+    if (WalletExist) {
+      return res.status(422).json({ error: "Wallet address already register" });
+    }
+
+    const wallet = new Wallet({
+      walletAddress,
+      requestAmount,
+      requestType,
+      time,
+    });
+
+    const walletAdd = await wallet.save();
+    if (walletAdd) {
+      return res.status(201).json({ message: "Wallet info add successfully" });
+    } else {
+      return res.status(500).json({ error: "Failed to add wallet info" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getWallet = async (_req, res) => {
+  try {
+    const allWalletInfo = await Wallet.find();
+    res.send(allWalletInfo);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  createWallet,
+  getWallet,
+};
---
+++ routes/secureRouters/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    createWallet,
+    getWallet
+} = require("../../controller/scureController/index");
+
+router.post("/create_wallet", createWallet);
+router.get("/get_user_wallet", getWallet);
+
